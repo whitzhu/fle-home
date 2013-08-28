b900b53a82e5b35d6715165ae77ef77bfd395b0a
Dylan Barth <dylan.j.barth@gmail.com>
2013-08-28 15:01:09 -0700

    Config for wsgi file

--- fle_site/wsgi.py
+++ fle_site/wsgi.py
@@ -1,28 +1,19 @@
-"""
-WSGI config for fle_site project.
-
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
+import sys
+import site
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fle_site.settings")
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/ubuntu/.virtualenvs/fle-site/local/lib/python2.7/site-packages/')
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/ubuntu/fle-site')
+sys.path.append('/home/ubuntu/fle-site/fle_site')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "fle_site.settings"
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/ubuntu/.virtualenvs/fle-site/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
