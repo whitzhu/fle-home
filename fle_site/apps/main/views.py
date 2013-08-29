d95a214b19ce74327b7334935ab3fa954967ed99
Dylan Barth <dylan.j.barth@gmail.com>
2013-08-29 15:37:42 -0700

    render helpful 500 page.

--- fle_site/apps/main/views.py
+++ fle_site/apps/main/views.py
@@ -1,13 +1,18 @@
 import pygeoip
 import json
 import re
+import sys
 
 from django.shortcuts import render_to_response
+from django.http import HttpResponseServerError
+from django.template.loader import render_to_string
+from django.template import RequestContext
 
 from fle_site import settings
 
 
 def home(request):
+	0/0
 	return render_to_response("home.html")
 
 def map(request): 
@@ -31,3 +36,11 @@ def map(request):
 	# remove duplicates and remove null coordinates
 	# location_info = list(set(location_info) - set([(0, 0)]))
 	return render_to_response('map.html', {"locations": json.dumps(locations)})
+
+def handler_500(request):
+    errortype, value, tb = sys.exc_info()
+    context = {
+        "errortype": errortype.__name__,
+        "value": str(value),
+    }
+    return HttpResponseServerError(render_to_string("500.html", context, context_instance=RequestContext(request)))
--- fle_site/urls.py
+++ fle_site/urls.py
@@ -21,3 +21,6 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT}))
+
+
+handler500 = 'fle_site.apps.main.views.handler_500'
