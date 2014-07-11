15693fbca03d2b8e8372607b5028ec771cd295e8
Jamie Alexandre <jamalex@gmail.com>
2014-07-11 14:14:07 -0700

    Redirect from old map URL to new map URL.

--- fle_site/apps/main/urls.py
+++ fle_site/apps/main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import TemplateView, RedirectView
 
 
 urlpatterns = patterns(__package__ + '.views',
-	url(r'^map/$', 'map', name='map'),
+    url(r'^map/$', RedirectView.as_view(url=reverse_lazy('map'))),
     url(r'^give/$', TemplateView.as_view(template_name='main/give.html'), name='give'),
     url(r'^directions/$', TemplateView.as_view(template_name='main/directions.html'), name='directions'),
 )
