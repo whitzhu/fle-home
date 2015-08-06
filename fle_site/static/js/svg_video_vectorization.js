91cafa542bb2a3234f0f84c888a06fe6959e6de6
66eli77 <66eli77@gmail.com>
2015-08-05 22:22:36 -0700

    modify the svg animations

--- fle_site/static/js/svg_video_vectorization.js
+++ fle_site/static/js/svg_video_vectorization.js
@@ -6,21 +6,20 @@ var vv_2_url = document.getElementById("vv_2").src;//a hack to get the image url
 var vv_1 = svg_vv.image(vv_1_url, 60, -100, 700, 700);
 var vv_2 = svg_vv.image(vv_2_url, 60, -100, 700, 700);
 
-var mask_rect = svg_vv.rect(0, -120, 400, 800);
-mask_rect.attr({fill: '#fff'});
+var moving_box = svg_vv.rect(0, -200, 400, 900).attr({fill: 'white', 'fill-opacity': 0.2, stroke: 'white', strokeWidth: 5});
 
-vv_2.attr({mask: mask_rect});
+var mask_1 = svg_vv.rect(-400, -200, 400, 800).attr({fill: '#fff'});
+var mask_2 = svg_vv.rect(400, -200, 400, 800).attr({fill: '#fff'});
+var mask_group = svg_vv.group(mask_1, mask_2);
 
-animate_mask_left(mask_rect, 3000);
+vv_2.attr({mask: mask_group});
 
-function animate_mask_left(element, dur) {
-    element.animate({width: 10}, dur, mina.easein, function(){
-        animate_mask_right(element, dur);
-    });
-}
+animate_box(moving_box, 400, 0);
+animate_box(mask_group.select('rect:nth-child(1)'), 0, -400);
+animate_box(mask_group.select('rect:nth-child(2)'), 800, 400);
 
-function animate_mask_right(element, dur) {
-    element.animate({width: 600}, dur, mina.elastic, function(){
-        animate_mask_left(element, dur);
+function animate_box(element, move1, move2) {
+    element.animate({x: move1}, 3000, function(){
+        animate_box(element, move2, move1);
     });
 }
