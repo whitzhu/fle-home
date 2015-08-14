ce5f9b6242b2ce620cc86920b823b8f31c5e573e
whitzhu <whitzhu@gmail.com>
2015-08-13 17:26:15 -0700

    Parallax Section

--- fle_site/static/js/_main.js
+++ fle_site/static/js/_main.js
@@ -30,33 +30,59 @@
 		}, 800);
 	});
 	
-	function adjustWindow(){
-		
-		// Init Skrollr
-		var s = skrollr.init({
-		    render: function(data) {
-		    
-		        //Debugging - Log the current scroll position.
-		        //console.log(data.curTop);
-		    }
-		});
-		
-		// Get window size
+
+function adjustWindow(){
+	     
+	    // Get window size
 	    winH = $window.height();
-	    
+	    winW = $window.width();
+	     
 	    // Keep minimum height 550
 	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*3);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
-	    
-	}
+	        winH = 550;
+	    }
+	     
+	    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init({
+	            forceHeight: false
+	        });
+	 
+	        // Resize our slides
+	        $slide.height(winH);
+	 
+	        s.refresh($('.homeSlide'));
+	 
+	    } else {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+	 
+	    // Check for touch
+	    if(Modernizr.touch) {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+ 
+}
+
+function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+}
+ 
+enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
 		
 } )( jQuery );
