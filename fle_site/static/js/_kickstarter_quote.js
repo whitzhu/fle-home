1933e4f96235ac5a90f42974d570ec45e3670354
66eli77 <66eli77@gmail.com>
2015-09-09 10:48:19 -0700

    add pause btn to quote section

--- fle_site/static/js/_kickstarter_quote.js
+++ fle_site/static/js/_kickstarter_quote.js
@@ -2,35 +2,30 @@ $(function() {
     var vid = document.getElementById("quote_video");
     vid.addEventListener("ended", resetVideo, false);
     var vid_btn = $('#quote_video_btn');
-    vid_btn.css("visibility", "visible");
-    var froze_rotate = false;
+    // vid_btn.css("visibility", "visible");
+    var is_play = false;
 
     vid_btn.bind("click", function() {
-        froze_rotate = true;
-        vid.play();
-        vid_btn.css("opacity", "0");
+        if(!is_play){
+            is_play = true;
+            vid.play();
+            vid_btn.attr('src', '/static/img/kickstarter/quote/pause-btn.png');
+            vid_btn.css("animation-iteration-count", "1");
+            vid_btn.css("opacity", "0.4");
+        }else{
+            is_play = false;
+            vid.pause();
+            vid_btn.attr('src', '/static/img/kickstarter/quote/audio-btn.png');
+            vid_btn.css("animation-iteration-count", "infinite");
+            vid_btn.css("opacity", "1");
+        }
     });
 
-    dim();
-
-    function dim() {
-        // vid_btn.css("opacity", "0");
-        vid_btn.css("background", "0");
-        setTimeout(function(){
-            lit();
-        }, 1200);
-    }
-
-    function lit() {
-        vid_btn.css("background", "rgba(134, 112, 161, 0.9)");
-        setTimeout(function(){
-            dim();
-        }, 1200);
-    }
-
     function resetVideo() {
-        froze_rotate = false;
+        is_play = false;
         vid.load();
+        vid_btn.attr('src', '/static/img/kickstarter/quote/audio-btn.png');
+        vid_btn.css("animation-iteration-count", "infinite");
         vid_btn.css("opacity", "1");
     }
 });
