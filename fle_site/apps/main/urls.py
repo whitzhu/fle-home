b0b52e3d2946bf35391b65671e6b7d0c9cb53796
Jamie Alexandre <jamalex@gmail.com>
2015-10-06 20:20:13 -0700

    Redirect from old prelaunch page to new URL.

--- fle_site/apps/main/urls.py
+++ fle_site/apps/main/urls.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import TemplateView, RedirectView
+from django.http import HttpResponseRedirect
 
 from .views import process_donation, file_upload
 
 urlpatterns = patterns(__package__ + '.views',
-	url(r'^$', TemplateView.as_view(template_name='main/homepage.html'), name='home'),
-	url(r'^indiegogo/$', TemplateView.as_view(template_name='main/kickstarter.html'), name='indiegogo'),
+    url(r'^$', TemplateView.as_view(template_name='main/homepage.html'), name='home'),
+    url(r'^kolibri/', lambda request: HttpResponseRedirect(reverse('indiegogo'))),
+    url(r'^indiegogo/$', TemplateView.as_view(template_name='main/kickstarter.html'), name='indiegogo'),
     url(r'^map/$', RedirectView.as_view(url=reverse_lazy('map'))),
     url(r'^give/$', RedirectView.as_view(url=reverse_lazy('donate')), name='give'),
     url(r'^donate/$', TemplateView.as_view(template_name='main/donate.html'), {"STRIPE_PUBLISHABLE_API_KEY": settings.STRIPE_PUBLISHABLE_API_KEY}, name='donate'),
