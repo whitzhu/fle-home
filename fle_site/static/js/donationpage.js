53fb63aa1e1ebde536578687c4d7ca2ba5133eb0
whitzhu <whitzhu@gmail.com>
2016-04-15 10:15:50 -0700

    Added customize checkbox for 'monthly giving'. Also cleaned up donationpage.js

--- fle_site/static/js/donationpage.js
+++ fle_site/static/js/donationpage.js
@@ -1,85 +1,73 @@
-$(document).ready(
- 
-  function() {
 
-    $("#other-amount").click(function() {
-      $("#input-amount").slideDown();
-    });
+//Donation Interface  
+$(function() {
+
+  //Other input field slides down
+  $("#other-amount").click(function() {
+    $("#input-amount").slideDown();
+  });
+
+  //Click of donation-amount buttons toggle button-pressed effect
+  $(".btn-amount").click(function(){
+   $(this).toggleClass('active').siblings().removeClass("active");
+  });
+
+  //Click stores value of preset $20, $50, $100 is stored 
+  $(".btn-amount-number").click(function(){
+   var amount = $(this).attr("value");
+   console.log(amount);
+  });
+
+  //Hides the other input box when $20, $50, $100 is selected
+  $(".btn-amount-number").click(function(){
+    $("#input-amount").hide();
+  });
+
+});
+
+//Stripe  
+$(function(){ 
+
+  var MonthlyGiving = false;
   
-    //Click of donation-amount buttons toggle button-pressed effect
-    $(".btn-amount").click(function(){
-     $(this).toggleClass('active').siblings().removeClass("active");
-    });
+  //Stripe Integration
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+    image: 'https://s3.amazonaws.com/stripe-uploads/acct_102ejd27dVLKpIVBmerchant-icon-141490-FLE-globe-only-logo.png' ,
+    locale: 'auto',
+    token: function(data) {
 
-    //Click stores value of preset $20, $50, $100 is stored 
-    $(".btn-amount-number").click(function(){
-     var amount = $(this).attr("value");
-     console.log(amount);
-    });
-  
-    $(".btn-amount-number").click(function(){
-      $("#input-amount").hide();
-    });
+              $.ajax({
+                  method: "POST",
+                  url: "/donate/process/",
+                  data: JSON.stringify(data),
+                  dataType: "json",
+                  contentType: "application/json",
+                  beforeSend: function(xhr, settings) {
+                      xhr.setRequestHeader("X-CSRFToken", data.csrfmiddlewaretoken);
+                  }
+              }).then(function(response) {
+                  show_message(response.status, response.message, "transaction-message");
+              });
+        }
+  });
 
-    // $("#btn-card").click(function(){
-      
-    //   if (  $(".active").val() == "custom"){
-    //     amount = $("#InputAmount").val();
-    //     if (amount.match(/^\d+$/)){
-    //       console.log(amount);
-    //     }else{
-    //       alert("Please Enter Valid Number");
-    //     }  
-    //   }else{
-    //     amount = $(".active").val();
-    //     console.log(amount);
-    //   }       
-  // });
 
-    //Stripe
-    var handler = StripeCheckout.configure({
-      key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-      image: 'https://s3.amazonaws.com/stripe-uploads/acct_102ejd27dVLKpIVBmerchant-icon-141490-FLE-globe-only-logo.png' ,
-      locale: 'auto',
-      token: function(data) {
 
-                $.ajax({
-                    method: "POST",
-                    url: "/donate/process/",
-                    data: JSON.stringify(data),
-                    dataType: "json",
-                    contentType: "application/json",
-                    beforeSend: function(xhr, settings) {
-                        xhr.setRequestHeader("X-CSRFToken", data.csrfmiddlewaretoken);
-                    }
-                }).then(function(response) {
-                    show_message(response.status, response.message, "transaction-message");
-                });
-          }
-    });
+  //Checkout process when 'Pay by Card' is selected
+  $('#btn-card').on('click', function(e) {
 
-    $('#btn-card').on('click', function(e) {
-      // Open Checkout with further options
-      
-      if (  $(".active").val() == "custom"){
-        amount = $("#InputAmount").val();
-        if (amount.match(/^\d+$/)){
-          console.log(amount);
-
-          handler.open({
-          name: 'Learning Equality',
-          description: 'Donation',
-          amount: amount*100,
-          panelLabel: "Give",
-          });
-          e.preventDefault();
-
-        }else{
-          alert("Please Enter Valid Number");
-        }  
-      }else{
-        amount = $(".active").val();
+    //Check for monthly giving
+    if (  $('#monthly-checkbox').prop('checked') ){
+      MonthlyGiving = true;
+      console.log('Monthy Giving');  //Check if monthly payment is selected
+    }
+    
+    if (  $(".active").val() == "custom"){
+      amount = $("#InputAmount").val();
+      if (amount.match(/^\d+$/)){
         console.log(amount);
+        console.log(MonthlyGiving);
 
         handler.open({
         name: 'Learning Equality',
@@ -88,33 +76,59 @@ $(document).ready(
         panelLabel: "Give",
         });
         e.preventDefault();
-      } 
+      }else{
+        alert("Please Enter Valid Number");
+      }  
+    }else{
+      amount = $(".active").val();
+      console.log(amount);
+      console.log(MonthlyGiving);
 
+      handler.open({
+      name: 'Learning Equality',
+      description: 'Donation',
+      amount: amount*100,
+      panelLabel: "Give",
+      });
+      e.preventDefault();
+    } 
+  });
 
-    });
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
+  });
 
-    // Close Checkout on page navigation
-    $(window).on('popstate', function() {
-      handler.close();
-    });
 });
+
+
+
+
+    
+
+
+
 
 //Circle Progress bar
-window.onload = function onLoad() {
+$(function(){
 
-var circle = new ProgressBar.Circle('#circle-stats', {
-    color: '#69C4EE',
-    strokeWidth: 3,
- 
-    text: { value:'100% Guarantee'}
+
+    window.onload = function onLoad() {
+
+    var circle = new ProgressBar.Circle('#circle-stats', {
+        color: '#69C4EE',
+        strokeWidth: 3,
+     
+        text: { value:'100% Guarantee'}
+    });
+
+    circle.animate(0.7, function() {
+        circle.animate(1);
+    });
+    }
 });
 
-circle.animate(0.7, function() {
-    circle.animate(1);
-})
 
-
-}
 
  
   
